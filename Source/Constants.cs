d5d62ee9d31c4055904754c7fc50bda0ccab09a9
Lisias T <me@lisias.net>
2018-06-05 20:04:09 -0300

    Fixing missing icons on UI - Creating the "file manager" to keep track of any file system demands

---
+++ Source/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ImpossibleInnovations
+{
+	/*
+	 * Constants Definition
+	 */
+	static class Constants
+	{
+		/* General rules:
+		 * + Directories *always* end witn "/".
+		 * + Pathnames *never* start with "/".
+		 * + Use Path.Combine where's possible.
+		 */
+		public const string GAMEDATA = "GameData/";
+		public const string PLUGINDATA = "PluginData/";
+		public const string LOCAL = "_LOCAL/";
+		public const string ROOT = "net.lisias.ksp/";
+		public const string BASE = "ImpossibleInnovations/";
+		public const string USERDATA = GAMEDATA + LOCAL;
+
+		public const string PLUGIN_ID = "ImpossibleInnovations";
+		public const string MANUFACTURER_NAME = "Impossible Innovations";
+	}
+}
--- Source/II_Icons.cs
+++ Source/II_Icons.cs
@@ -19,23 +19,23 @@ namespace ImpossibleInnovations
             Texture2D unselectedLegacy = new Texture2D(32, 32);
             Texture2D selectedLegacy = new Texture2D(32, 32);
 
-            unselected.LoadImage(File.ReadAllBytes("GameData/ImpossibleInnovations/Plugins/PluginData/SmallLogo.png"));
-            selected.LoadImage(File.ReadAllBytes("GameData/ImpossibleInnovations/Plugins/PluginData/SmallLogoON.png"));
+			unselected.LoadImage(File.ReadAllBytes(FileManager.FULLPATHNAME(FileManager.PLUGINDATA_PATHNAME, "SmallLogo.png")));
+			selected.LoadImage(File.ReadAllBytes(FileManager.FULLPATHNAME(FileManager.PLUGINDATA_PATHNAME, "SmallLogoON.png")));
             RUI.Icons.Selectable.Icon filterIcon = new RUI.Icons.Selectable.Icon("II_filter_icon", unselected, selected); //Defining filterIcon
 
-            unselectedLegacy.LoadImage(File.ReadAllBytes("GameData/ImpossibleInnovations/Plugins/PluginData/SmallLogoGrey.png"));
-            selectedLegacy.LoadImage(File.ReadAllBytes("GameData/ImpossibleInnovations/Plugins/PluginData/SmallLogoGreyON.png"));
+			unselectedLegacy.LoadImage(File.ReadAllBytes(FileManager.FULLPATHNAME(FileManager.PLUGINDATA_PATHNAME, "SmallLogoGrey.png")));
+			selectedLegacy.LoadImage(File.ReadAllBytes(FileManager.FULLPATHNAME(FileManager.PLUGINDATA_PATHNAME, "SmallLogoGreyON.png")));
             RUI.Icons.Selectable.Icon filterIconLegacy = new RUI.Icons.Selectable.Icon("II_filter_icon_legacy", unselectedLegacy, selectedLegacy); //Defining filterIconLegacy
 
-			PartCategorizer.Category IIfilter = PartCategorizer.AddCustomFilter("ImpossibleInnovations", "Impossible Innovations", filterIcon, Color.white);
+            PartCategorizer.Category IIfilter = PartCategorizer.AddCustomFilter(Constants.PLUGIN_ID, Constants.MANUFACTURER_NAME, filterIcon, Color.white);
 
             //filters for all II parts
-			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "AllParts", "All Impossible Innovations Parts", filterIcon, o => o.manufacturer == "Impossible Innovations" && !o.title.Contains("(LEGACY)"));
-			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Tanks", "Tanks", filterIcon, p => p.resourceInfos.Exists(q => q.resourceName == "Deuterium" || q.resourceName == "Tritium") && p.manufacturer == "Impossible Innovations");
-			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Engines", "Engines", filterIcon, r => r.title.Contains("Fusion Engine") && r.manufacturer == "Impossible Innovations");
-			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "BoostersCL20", "CL-20 Boosters", filterIcon, s => s.resourceInfos.Exists(t => t.resourceName == "CL-20") && s.manufacturer == "Impossible Innovations");
-			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "WingsIonized", "Ionized Wings", filterIcon, u => u.title.Contains("Ionized") && !u.title.Contains("(LEGACY)") && u.manufacturer == "Impossible Innovations");
-			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Legacy", "Legacy Parts", filterIconLegacy, v => v.title.Contains("(LEGACY)") && v.manufacturer == "Impossible Innovations");
+			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "AllParts", string.Format("All {0} Parts", Constants.MANUFACTURER_NAME), filterIcon, o => o.manufacturer == Constants.MANUFACTURER_NAME && !o.title.Contains("(LEGACY)"));
+			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Tanks", "Tanks", filterIcon, p => p.resourceInfos.Exists(q => q.resourceName == "Deuterium" || q.resourceName == "Tritium") && p.manufacturer == Constants.MANUFACTURER_NAME);
+			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Engines", "Engines", filterIcon, r => r.title.Contains("Fusion Engine") && r.manufacturer == Constants.MANUFACTURER_NAME);
+			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "BoostersCL20", "CL-20 Boosters", filterIcon, s => s.resourceInfos.Exists(t => t.resourceName == "CL-20") && s.manufacturer == Constants.MANUFACTURER_NAME);
+			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "WingsIonized", "Ionized Wings", filterIcon, u => u.title.Contains("Ionized") && !u.title.Contains("(LEGACY)") && u.manufacturer == Constants.MANUFACTURER_NAME);
+			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Legacy", "Legacy Parts", filterIconLegacy, v => v.title.Contains("(LEGACY)") && v.manufacturer == Constants.MANUFACTURER_NAME);
         }
 
         private void Awake()
