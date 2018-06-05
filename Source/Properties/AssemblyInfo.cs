14440b2ede8b71e06e705948c9b131c7b3299b67
Lisias T <me@lisias.net>
2018-06-05 20:04:43 -0300

    bumping version. normalizing versioning information

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("ImpossibleInovations")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle (ImpossibleInnovations.Constants.PLUGIN_ID)]
+[assembly: AssemblyDescription ("Unofficial Build.")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
+[assembly: AssemblyCompany ("net.lisias.ksp")]
 [assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright ("jandcando")]
 [assembly: AssemblyTrademark ("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.8.7.7")]
+[assembly: AssemblyVersion (ImpossibleInnovations.Version.Number)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
---
+++ Source/Version.cs
@@ -0,0 +1,12 @@
+﻿namespace ImpossibleInnovations
+{
+	static class Version
+	{
+		public const int major = 0;
+		public const int minor = 8;
+		public const int patch = 7;
+		public const int build = 8;
+		public const string Number = "0.8.7.8";
+		public const string Text = Number + " Unofficial";
+	}
+}
