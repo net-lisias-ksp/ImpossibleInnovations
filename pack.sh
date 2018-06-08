d314e281a2c61623f9ac4daa24fc18e742884170
Lisias T <me@lisias.net>
2018-06-08 00:37:47 -0300

    Adding scripts for automation

---
+++ pack.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+VERSION=$(cat ./Source/Version.cs | grep -Po 'public const string Number = "(.+)";' | sed 's/public const string Number = "//' | sed 's/";//')
+echo $VERSION
+FILE=Impossible_Innovations-$VERSION.zip
+rm $FILE
+zip -r $FILE ./GameData/* -x ".*"
+zip -d $FILE __MACOSX .DS_Store
+mv $FILE ./Archive
