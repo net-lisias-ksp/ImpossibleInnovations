cfc8be6dc9798346a3ae8f508516aa079eea08c1
Lisias T <me@lisias.net>
2018-10-10 04:31:11 -0300

    Legal mambo-jambo

--- Source/ImpossibleInnovations/II_Icons.cs
+++ Source/ImpossibleInnovations/II_Icons.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+	This file is part of Impossible Innovations,
+		(C) 2014-2018 : JandCandO https://spacedock.info/profile/jandcando
+		(C) 2018 Lisias T : http://lisias.net <support@lisias.net>
+
+	Impossible Innovations is as follows:
+
+		* CC BY-NC-SA 4.0 : https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+	And you are allowed to choose the License that better suit your needs.
+
+	Impossible Innovations is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with Impossible Innovations. If not, see < https://creativecommons.org/>.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
@@ -46,6 +64,7 @@ namespace ImpossibleInnovations
 			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "CommandPods", "Command Pods", filterIcon, o => o.manufacturer == Constants.MANUFACTURER_NAME && o.category.ToString() == "Pods" && !o.title.Contains("(LEGACY)"));
 			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Control", "Control", filterIcon, o => o.manufacturer == Constants.MANUFACTURER_NAME && o.category.ToString().Contains("Control") && !o.title.Contains("(LEGACY)"));
 			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Tanks", "Tanks", filterIcon, p => p.resourceInfos.Exists(q => q.resourceName == "HydrogenProtium" || q.resourceName == "HydrogenDeuterium" || q.resourceName == "HydrogenTritium") && p.manufacturer == Constants.MANUFACTURER_NAME);
+			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Electrical", "Electrical", filterIcon, o => o.manufacturer == Constants.MANUFACTURER_NAME && o.category.ToString() == "Electrical" && !o.title.Contains("(LEGACY)"));
 			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "Engines", "Engines", filterIcon, r => r.title.Contains("Fusion Engine") && r.manufacturer == Constants.MANUFACTURER_NAME);
 			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "BoostersCL20", "CL-20 Boosters", filterIcon, s => s.resourceInfos.Exists(t => t.resourceName == "CL-20") && s.manufacturer == Constants.MANUFACTURER_NAME);
 			PartCategorizer.AddCustomSubcategoryFilter(IIfilter, "WingsIonized", "Ionized Wings", filterIcon, u => u.title.Contains("Ionized") && !u.title.Contains("(LEGACY)") && u.manufacturer == Constants.MANUFACTURER_NAME);
--- Source/ImpossibleInnovations/ModuleIIIonWing.cs
+++ Source/ImpossibleInnovations/ModuleIIIonWing.cs
@@ -1,4 +1,22 @@
-﻿using UnityEngine;
+﻿/*
+	This file is part of Impossible Innovations,
+		(C) 2014-2018 : JandCandO https://spacedock.info/profile/jandcando
+		(C) 2018 Lisias T : http://lisias.net <support@lisias.net>
+
+	Impossible Innovations is as follows:
+
+		* CC BY-NC-SA 4.0 : https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+	And you are allowed to choose the License that better suit your needs.
+
+	Impossible Innovations is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with Impossible Innovations. If not, see < https://creativecommons.org/>.
+*/
+using UnityEngine;
 
 namespace ImpossibleInnovations
 {
