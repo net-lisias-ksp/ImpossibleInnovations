114c2c502cc821a95c624fbf995ee12fda5bf123
Lisias <me@lisias.net>
2020-07-23 08:28:32 -0300

    Legal Mambo Jambo

--- Source/ImpossibleInnovations/ModuleIIIonWing.cs
+++ Source/ImpossibleInnovations/ModuleIIIonWing.cs
@@ -2,23 +2,30 @@
 	This file is part of Impossible Innovations,
 		(C) 2018-2020 : Lisias T : http://lisias.net <support@lisias.net>
 		(C) 2014-2018 : JandCandO https://spacedock.info/profile/jandcando
+	and it's doubled licensed to you under SKL-1.0 & GPL-2.0
 
 	Impossible Innovations is licensed as follows:
 
-		* CC BY-NC-SA 4.0 : https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-	And you are allowed to choose the License that better suit your needs.
+	* Source Code and TweakScale Patches:
+		+ SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+		+ GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+		+ And you are allowed to choose the License that better suit your needs.
+	* Models, Textures, Art & Configs:
+		+ CC BY-NC-SA 4.0
 
 	Impossible Innovations is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-	You should have received a copy of the CC BY-NC-SA 4.0
-	along with Impossible Innovations. If not, see < https://creativecommons.org/>.
-*/
-using System;
-using UnityEngine;
+	You should have received a copy of the SKL Standard License 1.0
+	along with Impossible Innovations. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
 
+	You should have received a copy of the GNU General Public License 2.0
+	along with Impossible Innovations. If not, see <https://www.gnu.org/licenses/>.
+
+	You should have received a copy of the Creative Commons 4.0 Attribution-NonCommercial-ShareAlike License
+	along with Impossible Innovations. If not, see <https://creativecommons.org/licenses/by-nc-sa/4.0/>.
+*/
 namespace ImpossibleInnovations
 {
 	public class ModuleIIIonWing : PartModule
